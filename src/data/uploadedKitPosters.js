8ceb135f383111e287e278c532521929df64f8ea
1136560307 <59765447+1136560307@users.noreply.github.com>
2026-06-12 18:08:25 +0800

    Add uploaded kit poster data

---
+++ src/data/uploadedKitPosters.js
@@ -0,0 +1,9 @@
+export const uploadedKitPosters = [
+  {
+    title: '植萃人参虫草首乌精华液套盒',
+    spec: '白发养护 · 发根滋养',
+    tag: '院线套盒',
+    image: 'data:image/webp;base64,PLACEHOLDER',
+    href: '/all-product/27taohe/',
+  },
+];
